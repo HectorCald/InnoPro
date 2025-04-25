77695ede9b508839aa37da4685fd0520b322ba21
Hector Cald <hi.hector20@gmail.com>
2025-04-25 13:13:42 -0400

    usuario y pin verioficacion

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ async function verificarPin(pin) {
 const APP_CONFIG = {
     UPDATE_KEY: 'innopro_update_status',
     CURRENT_VERSION: '2.0.6',
-    MIN_VERSION: '2.0.5'
+    MIN_VERSION: '2.0.4'
 };
 
 /* ==================== RUTAS DE VISTAS ==================== */
